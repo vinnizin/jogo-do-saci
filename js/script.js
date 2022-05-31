ded26592a3bb854edd1d744ffcb2939425c5c2e9
vinnizin <vinniaguilar10@gmail.com>
2022-05-31 09:04:49 -0300

    JOGO DO SACI

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const saci = document.querySelector('.saci');
+const velhovamp = document.querySelector('.velhovamp');
+const nuvens = document.querySelector('.nuvens');
+
+const jump = () => {
+    saci.classList.add('jump')
+
+    setTimeout(() => {
+        saci.classList.remove('jump')
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const velhovampPosition = velhovamp.offsetLeft;
+    const saciPosition = +window.getComputedStyle(saci).bottom.replace('px','');
+    
+    if (velhovampPosition <= 120 && velhovampPosition > 0 && saciPosition < 80) {
+
+        velhovamp.style.animation = 'none';
+        velhovamp.style.left = `${velhovampPosition}px`;
+
+        saci.style.animation = 'none';
+        saci.style.bottom = `${saciPosition}px`;
+
+        saci.src = './images/saci-gameover.png'
+        saci.style.width = '150px'
+        saci.style.marginLeft= '20px'
+
+
+        clearInterval(loop);
+
+    }
+
+}, 10);
+
+document.addEventListener('keydown', jump);
