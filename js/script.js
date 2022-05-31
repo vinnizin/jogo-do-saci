1176774671c715c7d6810fe3b7cde179803c4081
vinnizin <vinniaguilar10@gmail.com>
2022-05-31 18:34:02 -0300

    Algumas mudanças

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const saci = document.querySelector('.saci');
 const velhovamp = document.querySelector('.velhovamp');
 const velhovampdois = document.querySelector('.velhovampdois');
 const nuvens = document.querySelector('.nuvens');
+const gameover = document.querySelector('.gameover');
+
 
 const jump = () => {
     saci.classList.add('jump')
@@ -29,6 +31,36 @@ const loop = setInterval(() => {
         saci.style.width = '150px'
         saci.style.marginLeft= '20px'
 
+        var gameover = document.createElement('img');
+        var elemento = document.getElementsByClassName ('gameover');
+        elemento.insertAdjacentHTML(afterbegin, gameover);
+
+
+        clearInterval(loop);
+
+
+    }
+
+}, 10);
+
+const loopvelhodois = setInterval(() => {
+
+    const velhovampdoisPosition = velhovampdois.offsetLeft;
+    const saciPosition = +window.getComputedStyle(saci).bottom.replace('px','');
+    
+    if (velhovampdoisPosition <= 120 && velhovampdoisPosition > 0 && saciPosition > 80) {
+
+        velhovampdois.style.animation = 'none';
+        velhovampdois.style.left = `${velhovampdoisPosition}px`;
+
+        saci.style.animation = 'none';
+        saci.style.bottom = `${saciPosition}px`;
+
+        saci.src = './images/saci-gameover.png'
+        saci.style.width = '150px'
+        saci.style.marginLeft= '20px'
+
+        gameover.src = '../images/tela-gameover.png'
 
         clearInterval(loop);
 
